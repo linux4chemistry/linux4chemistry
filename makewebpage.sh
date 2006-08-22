d4bdabc839ddc03af42ea5f6d8749d47094fd534
baoilleach <baoilleach@f392dc91-a219-0410-aec9-5111d4c6a560>
2006-08-22 14:58:25 +0000

    Major shakeup of the repository. Now you can recreate the website by running makewebpage.sh.

---
+++ makewebpage.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+dist=website
+
+rm -r $dist
+mkdir $dist
+
+cp src/linux4chemistry.py $dist
+cp src/leavecomment.py $dist
+cp data/l4c.txt $dist/tablinux.txt
+cp src/definition.html $dist
+cp src/style.css $dist
+cp img/*.png $dist
+mkdir $dist/email
+cp src/carlo_nervi.* $dist/email
+
+cd $dist
+python linux4chemistry.py | tail +3 | sed 's/name="state" value="0"/name="state" value="1"/g' > index.html
+python leavecomment.py | tail +3 | sed 's/name="state" value="0"/name="state" value="1"/g' > leavecomment.html
--- src/makeindex.sh
+++
@@ -1,1 +0,0 @@
-python linux4chemistry.py | tail +3 | sed 's/name="state" value="0"/name="state" value="1"/g' > index.html
