f16e782abe9679232ea7ed09e09966377e571926
Riccardo Vianello <riccardo.vianello@gmail.com>
2013-03-04 23:00:59 +0100

    more openshift settings

--- linux4chemistry/linux4chemistry/settings/openshift.py
+++ linux4chemistry/linux4chemistry/settings/openshift.py
@@ -28,5 +28,7 @@ SECRET_KEY = secret.SECRET_KEY
  
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'devel-l4c.rhcloud.com',
+    ]
 
