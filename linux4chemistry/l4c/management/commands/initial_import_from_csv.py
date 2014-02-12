3a37fa62940ece55a44c933296dbce73429cedc8
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-02-12 23:12:58 +0100

    minor improvements to the Software model

--- linux4chemistry/l4c/management/commands/initial_import_from_csv.py
+++ linux4chemistry/l4c/management/commands/initial_import_from_csv.py
@@ -8,15 +8,18 @@ from django.core.management.base import BaseCommand, CommandError
 import oldsite
 from l4c.models import Software
 
+
 Record = collections.namedtuple(
     'Record', [
-        'name', 'url', 'category', 'additional_category', 
+        'name', 'url', 'categories', 'other_categories', 
         'license_model', 'open_source_info', 
         'description', 'comments'
         ]
     )
 
+
 class Command(BaseCommand):
+
     help = (
         'Initial import of the historical data from the CSV document into ' +
         'the relational backend'
@@ -24,13 +27,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        db_license_models = dict(
+            (display_name, db_value) 
+            for (db_value, display_name) 
+            in Software.LICENSE_MODELS._display_map.items()
+            )
+ 
         filepath = os.path.join(os.path.dirname(oldsite.__file__),
                                 'data', 'l4c.txt')
 
         with open(filepath, 'rb') as csvfile:
             csvdata = csv.reader(csvfile, delimiter='\t')
+            csvdata.next() # skip header
             for record in itertools.imap(Record._make, csvdata):
-                Software.objects.create(**record._asdict())
+                model_data = record._asdict()
+                license_model = db_license_models[model_data['license_model']]
+                model_data['license_model'] = license_model
+                Software.objects.create(**model_data)
 
 
                     
--- linux4chemistry/l4c/models.py
+++ linux4chemistry/l4c/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
+from model_utils import Choices
+
 class Software(models.Model):
-    
+
+    LICENSE_MODELS = Choices(
+        ('open_source', 'Open Source'),
+        ('freeware', 'Freeware'),
+        ('academic', 'Free for academics'),
+        ('shareware', 'Shareware'),
+        ('commercial', 'Commercial'),
+        )
+
     name = models.CharField(max_length=50)
     url = models.URLField()
-    category = models.CharField(max_length=80)
-    additional_category = models.CharField(max_length=50)
-    license_model = models.CharField(max_length=40)
+    categories = models.CharField(max_length=80)
+    other_categories = models.CharField(max_length=50)
+    license_model = models.CharField(choices=LICENSE_MODELS, max_length=40)
     open_source_info = models.CharField(max_length=100) 
     description = models.TextField() 
     comments = models.TextField()
+
+    def __unicode__(self):
+        return self.name
