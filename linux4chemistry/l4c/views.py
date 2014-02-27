05884ab806e8d431984ba2b782f6f0282105c265
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-02-27 22:47:58 +0100

    api support for filtering the Software list based on license model and category

--- linux4chemistry/l4c/views.py
+++ linux4chemistry/l4c/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets
 
 from .models import LicenseModel, Category, Software
@@ -17,7 +18,38 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class SoftwareViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Software.objects.all().prefetch_related('categories')
+    model = Software
     serializer_class = SoftwareSerializer
-    
+
+    def get_queryset(self):
+        # support filtering the Software queryset based on an optional 
+        # subset of license models and/or software categories. 'other' 
+        # can be used as a special software category identifier.
+
+        queryset = self.model.objects.all()
+
+        license_param = self.request.QUERY_PARAMS.get('license_model', '')
+        license_models = set(license_param.split(',') if license_param else [])
+
+        if license_models:
+            queryset = queryset.filter(license_model__in=license_models)
+
+        category_param = self.request.QUERY_PARAMS.get('category', '')
+        categories = set(category_param.split(',') if category_param else [])
+
+        other = 'other' in categories
+        q1 = ~Q(other_categories='') if other else None
+
+        categories.discard('other')
+        q2 = Q(categories__in=categories) if categories else None
+
+        if q1 and q2:
+            queryset = queryset.filter(q1 | q2)
+        elif q1:
+            queryset = queryset.filter(q1)
+        elif q2:
+            queryset = queryset.filter(q2)
+
+        return queryset.prefetch_related('categories')
+
 
