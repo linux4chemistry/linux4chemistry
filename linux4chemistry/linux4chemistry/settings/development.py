1a5f6ab208f363e7ec1d7e41bb9be05c8c345aeb
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-07-21 22:02:01 +0200

    cleanup

--- linux4chemistry/linux4chemistry/settings/development.py
+++ linux4chemistry/linux4chemistry/settings/development.py
@@ -19,9 +19,10 @@ INSTALLED_APPS += (
     'debug_toolbar',
 )
 
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
+## deprecated
+#DEBUG_TOOLBAR_CONFIG = {
+#    'INTERCEPT_REDIRECTS': False,
+#}
 
 INTERNAL_IPS = ('127.0.0.1',)
 
