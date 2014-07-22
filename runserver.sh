1c7a630b5b4b841f339d77f6de2e2c7098a30e92
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-07-22 22:22:01 +0200

    preparing to run uwsgi in development too

---
+++ runserver.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo `dirname $0`
+
+export OPENSHIFT_DIY_IP=0.0.0.0
+export OPENSHIFT_DIY_PORT=8000
+export OPENSHIFT_REPO_DIR="`dirname $0`/"
+export OPENSHIFT_DATA_DIR=${OPENSHIFT_REPO_DIR}../dev-data/
+export OPENSHIFT_DIY_LOG_DIR=${OPENSHIFT_DATA_DIR}
+
+uwsgi --ini ${OPENSHIFT_REPO_DIR}uwsgi.ini
