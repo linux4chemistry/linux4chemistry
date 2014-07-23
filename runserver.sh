4dfdab664e08d02ab81bb66b103a261ebd6c16a3
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-07-23 22:57:14 +0200

    moving uwsgi ini script into its own directory

--- runserver.sh
+++ runserver.sh
@@ -6,4 +6,4 @@ export OPENSHIFT_REPO_DIR="`dirname $0`/"
 export OPENSHIFT_DATA_DIR=${OPENSHIFT_REPO_DIR}../dev-data/
 export OPENSHIFT_DIY_LOG_DIR=${OPENSHIFT_DATA_DIR}
 
-uwsgi --ini ${OPENSHIFT_REPO_DIR}uwsgi.ini
+uwsgi --ini ${OPENSHIFT_REPO_DIR}uwsgi/uwsgi.ini
