2affb9fe4ed53fb8d5c08d6547747597d0579b89
Riccardo Vianello <riccardo.vianello@gmail.com>
2014-07-24 00:08:36 +0200

    specialized uwsgi configuration for development and production envs.

--- runserver.sh
+++ runserver.sh
@@ -6,4 +6,6 @@ export OPENSHIFT_REPO_DIR="`dirname $0`/"
 export OPENSHIFT_DATA_DIR=${OPENSHIFT_REPO_DIR}../dev-data/
 export OPENSHIFT_DIY_LOG_DIR=${OPENSHIFT_DATA_DIR}
 
-uwsgi --ini ${OPENSHIFT_REPO_DIR}uwsgi/uwsgi.ini
+uwsgi \
+    --ini ${OPENSHIFT_REPO_DIR}uwsgi/uwsgi-common.ini \
+    --ini ${OPENSHIFT_REPO_DIR}uwsgi/uwsgi-development.ini
